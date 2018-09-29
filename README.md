5c35af645fae98a7663fe97508806ce2b92d9888
Brian Hammond <brian@fictorial.com>
2018-09-28 23:29:48 -0400

    Minor cleanup

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Socket.io server and client that emits messages when any of a number
 of tailed UTF-8 encoded files has some data appended to it.
 
+```bash
+node index.js access=/var/log/nginx/access.log error=/var/log/nginx/error.log
+```
--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+const fs = require('fs')
+
 process.argv.slice(2).forEach(keyAndPath => {
   let [ key, path ] = keyAndPath.split('=')
-  console.log(key, path)
-  if (!key || !path || !require('fs').existsSync(path)) throw new Error('args: key=path ...')
+  if (!key || !path || !fs.existsSync(path)) throw new Error('args: key=path ...')
   require('child_process').spawn('tail', ['-f', path]).stdout
     .on('data', data => io.emit('line', { key, data: data.toString('utf8').trim() }))
 })
 
 const app = require('http').createServer((req, res) => {
-  require('fs').readFile(__dirname + '/index.html', (err, data) => {
+  fs.readFile(__dirname + '/index.html', (err, data) => {
     if (err) { res.writeHead(500); res.end()     }
     else     { res.writeHead(200); res.end(data) }
   })
 })
 
 const io = require('socket.io')(app)
-app.listen(9090)
+app.listen(process.env.PORT || 9090)
