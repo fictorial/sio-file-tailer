de4606ec0612835be7090b9b140f7d8eb0f18b86
Brian Hammond <brian@fictorial.com>
2018-09-28 23:38:00 -0400

    Clean up

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const fs = require('fs')
 process.argv.slice(2).forEach(keyAndPath => {
   let [ key, path ] = keyAndPath.split('=')
   require('child_process').spawn('tail', ['-f', path]).stdout.on('data', data => io.emit('line', { key, data: data.toString('utf8').trim() }))
 })
-const app = require('http').createServer((_, res) => fs.readFile(`${__dirname}/index.html`, (_, data) => res.end(data)))
+const app = require('http').createServer((_, res) => require('fs').readFile(`${__dirname}/index.html`, (_, data) => res.end(data)))
 const io = require('socket.io')(app)
 app.listen(process.env.PORT || 9090)
